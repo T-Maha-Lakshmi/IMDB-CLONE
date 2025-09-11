87cd9b02b3bb3a8a990afbe6930441dbf417c74f
T-Maha-Lakshmi <mahalakshmitirumala6@gmail.com>
2025-09-11 17:19:56 +0530

    Setup router with Movies and Watchlist

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Logo from '../MovieLogo.png'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className='flex border space-x-8 items-centre pl-3 py-4'>
         <img className='w-[50px]' src={Logo} alt=''/>
-        <a href='/' className='text-blue-500 text-2xl font-bold'>Home</a>
+        <Link to='/' className='text-blue-500 text-2xl font-bold'>Movies</Link>
 
-        <a href='/watchlist' className='text-blue-500 text-2xl font-bold'>Watchlist</a>
+        <Link to='/watchlist' className='text-blue-500 text-2xl font-bold'>Watchlist</Link>
 
     </div>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import WatchList from '../components/WatchList'
+import Movies from '../components/Movies'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div>
-      
+    <BrowserRouter> 
       <Navbar/>
+    <Routes>
+      <Route path='/' element={<Movies/>}/>
+      <Route path='/watchlist' element={<WatchList/>}/>
+
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
