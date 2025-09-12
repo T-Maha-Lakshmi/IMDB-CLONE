87690696b9590dbb2c961fd3fe2a09168bdab16d
T-Maha-Lakshmi <mahalakshmitirumala6@gmail.com>
2025-09-12 21:16:18 +0530

    Added Movies and MovieCard components with API integration

---
+++ components/MovieCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function MovieCard({ poster_path , name}) {
+  return (
+    <div
+      className="relative h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 hover:cursor-pointer"
+      style={{
+        backgroundImage: `url(https://image.tmdb.org/t/p/w500${poster_path})`
+      }}
+    >
+      <div className='absolute bottom-0 text-white text-xl w-full p-2 text-center bg-gray-900/60'>
+        {name}
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
--- components/Movies.jsx
+++ components/Movies.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import MovieCard from './MovieCard'
+import axios from 'axios'
 
 function Movies() {
+  const [movies, setMovies] = useState([]); 
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/movie/popular?api_key=14d3e48edf0968edee100f93f3b592b8&language=en-US&page=1'
+      )
+      .then((res) => {
+        console.log(res.data.results); // log movies
+        setMovies(res.data.results); // store movies in state
+      })
+  }, []);
+
   return (
-    <div>
-        <h1>This is movies component.</h1>
+    <div className="p-4">
+      <div className="text-2xl m-5 font-bold text-center">
+        Trending Movies
+      </div>
+      <div className="flex flex-row flex-wrap justify-around gap-8">
+        {movies.map((movieObj)=>{
+          return <MovieCard poster_path={movieObj.poster_path} name={movieObj.original_title}/>
+        })}
+      </div>
     </div>
   )
 }
